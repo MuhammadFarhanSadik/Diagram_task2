332c5d410cff9f435a9608a1fa7b5c7200b74831
Muhammad Farhan Sadik <farhanshihab008@gmail.com>
2025-05-31 12:13:35 +0600

    Add files via upload

--- HotelManagement.java
+++ HotelManagement.java
@@ -9,57 +9,57 @@ public class HotelManagement {
         Hotel hotel = new Hotel("Sea View Hotel");
 
         // Add rooms
-        hotel.addRoom(new Room(101, "Single"));
-        hotel.addRoom(new Room(102, "Double"));
-        hotel.addRoom(new Room(103, "Suite"));
+        hotel.addGhor(new Room(101, "Single"));
+        hotel.addGhor(new Room(102, "Double"));
+        hotel.addGhor(new Room(103, "Suite"));
 
         // Create customer
-        Customer customer = new Customer("Mr. Foishal", "017xxxxxxxx");
+        Customer grohok = new Customer("Farhan", "017xxxxxxxx");
 
         // Search for available room
-        Room room = hotel.findAvailableRoom("Double");
+        Room ghor = hotel.khulaGhorKhujun("Double");
 
-        if (room != null) {
+        if (ghor != null) {
             // Create reservation
-            Date checkIn = new Date(); // current date
-            Date checkOut = new Date(checkIn.getTime() + 2 * 24 * 60 * 60 * 1000L); // 2 days later
+            Date dhukerTarikh = new Date(); // now
+            Date berHobarTarikh = new Date(dhukerTarikh.getTime() + 2 * 24 * 60 * 60 * 1000L); // 2 days later
 
-            Reservation reservation = new Reservation(customer, room, checkIn, checkOut);
+            Reservation booking = new Reservation(grohok, ghor, dhukerTarikh, berHobarTarikh);
 
-            hotel.addReservation(reservation);
-            room.setAvailable(false);
+            hotel.addBooking(booking);
+            ghor.setKhaliAche(false);
 
-            System.out.println("Room " + room.roomNumber + " booked successfully for " + customer.name);
+            System.out.println("Ghor " + ghor.ghorNumber + " booked for " + grohok.naam);
         } else {
-            System.out.println("No available room of requested type.");
+            System.out.println("Dhoroner kono khali ghor nei.");
         }
     }
 }
 
 // Hotel class
 class Hotel {
-    String name;
-    List<Room> rooms;
-    List<Reservation> reservations;
+    String naam;
+    List<Room> ghorShomuho;
+    List<Reservation> bookingShomuho;
 
-    public Hotel(String name) {
-        this.name = name;
-        this.rooms = new ArrayList<>();
-        this.reservations = new ArrayList<>();
+    public Hotel(String naam) {
+        this.naam = naam;
+        this.ghorShomuho = new ArrayList<>();
+        this.bookingShomuho = new ArrayList<>();
     }
 
-    public void addRoom(Room room) {
-        rooms.add(room);
+    public void addGhor(Room ghor) {
+        ghorShomuho.add(ghor);
     }
 
-    public void addReservation(Reservation reservation) {
-        reservations.add(reservation);
+    public void addBooking(Reservation booking) {
+        bookingShomuho.add(booking);
     }
 
-    public Room findAvailableRoom(String type) {
-        for (Room room : rooms) {
-            if (room.type.equalsIgnoreCase(type) && room.isAvailable) {
-                return room;
+    public Room khulaGhorKhujun(String dhoron) {
+        for (Room ghor : ghorShomuho) {
+            if (ghor.dhoron.equalsIgnoreCase(dhoron) && ghor.khaliAche) {
+                return ghor;
             }
         }
         return null;
@@ -68,43 +68,43 @@ class Hotel {
 
 // Room class
 class Room {
-    int roomNumber;
-    String type;
-    boolean isAvailable;
+    int ghorNumber;
+    String dhoron;
+    boolean khaliAche;
 
-    public Room(int roomNumber, String type) {
-        this.roomNumber = roomNumber;
-        this.type = type;
-        this.isAvailable = true;
+    public Room(int ghorNumber, String dhoron) {
+        this.ghorNumber = ghorNumber;
+        this.dhoron = dhoron;
+        this.khaliAche = true;
     }
 
-    public void setAvailable(boolean status) {
-        this.isAvailable = status;
+    public void setKhaliAche(boolean obostha) {
+        this.khaliAche = obostha;
     }
 }
 
 // Reservation class
 class Reservation {
-    Customer customer;
-    Room room;
-    Date checkIn;
-    Date checkOut;
+    Customer grohok;
+    Room ghor;
+    Date dhukerTarikh;
+    Date berHobarTarikh;
 
-    public Reservation(Customer customer, Room room, Date checkIn, Date checkOut) {
-        this.customer = customer;
-        this.room = room;
-        this.checkIn = checkIn;
-        this.checkOut = checkOut;
+    public Reservation(Customer grohok, Room ghor, Date dhukerTarikh, Date berHobarTarikh) {
+        this.grohok = grohok;
+        this.ghor = ghor;
+        this.dhukerTarikh = dhukerTarikh;
+        this.berHobarTarikh = berHobarTarikh;
     }
 }
 
 // Customer class
 class Customer {
-    String name;
-    String contactInfo;
+    String naam;
+    String jogajogTothyo;
 
-    public Customer(String name, String contactInfo) {
-        this.name = name;
-        this.contactInfo = contactInfo;
+    public Customer(String naam, String jogajogTothyo) {
+        this.naam = naam;
+        this.jogajogTothyo = jogajogTothyo;
     }
 }
